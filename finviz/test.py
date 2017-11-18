e066ef5ea0597787ff00911f9ae83dfb26bb8399
mariostoev <bg.mstoev@gmail.com>
2017-11-18 20:20:56 +0200

    Delete test.py

--- finviz/test.py
+++
@@ -1,7 +0,0 @@
-import public
-
-fv = public.Finviz()
-
-print(fv.search(filters=['cap_microunder', 'earningsdate_tomorrow', 'exch_nasd'], sort_by='country'))
-
-# //*[@id="screener-content"]/table/tbody/tr[4]/td/table/tbody/tr[2]/td[1]/a
