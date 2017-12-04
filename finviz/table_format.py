e93b905db1b65ad6d302a0c0c11b77d63370a0fa
mariostoev <bg.mstoev@gmail.com>
2017-12-04 17:58:18 +0200

    Update table_format.py

--- finviz/table_format.py
+++ finviz/table_format.py
@@ -1,4 +1,8 @@
-table_format = [['Number', 'Ticker', 'Company', 'Sector',
+table_format = [['No.', 'Ticker', 'Company', 'Sector',
                 'Industry', 'Country', 'Market Cap', 'P/E',
                 'Price', 'Change', 'Volume'],
-                ['Number']]
+                ['No.', 'Ticker', 'Market Cap', 'P/E',
+                 'Fwd P/E', 'PEG', 'P/S', 'P/B',
+                 'P/C', 'P/FCF', 'EPS this Y', 'EPS next Y',
+                 'EPS past 5Y', 'Sales past 5Y', 'Price',
+                 'Change', 'Volume']]
