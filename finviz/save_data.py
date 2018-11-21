ac28e7acd7e098ca26810425131cde8aead5c516
Mario Stoev <bg.mstoev@gmail.com>
2018-11-21 12:53:51 +0200

    Styling corrections, to match the other files

--- finviz/save_data.py
+++ finviz/save_data.py
@@ -4,13 +4,14 @@ import sqlite3
 import re
 
 def create_connection():
+    
     sqlite_file = "../screener.sqlite"
+    
     try:
         conn = sqlite3.connect(sqlite_file)
         return conn
-    except:
-        print("Error connecting to DB")
-        return None
+    except sqlite3.Error as error:
+        raise ("An error has occurred while connecting to the database: ", error.args[0])
 
 def export_to_csv(headers, data, directory):
 
@@ -22,41 +23,54 @@ def export_to_csv(headers, data, directory):
             dict_writer.writerows(n)
 
 def export_to_db(headers, data):
+
     field_list = ""
     table_name = "screener_results"  # name of the table to be created
     conn = create_connection()
     c = conn.cursor()
 
     for field in headers:
-        field_cleaned = re.sub(r'[^\w\s]','',field)
-        field_cleaned = field_cleaned.replace(" ", "")
-        field_list +=  field_cleaned + " TEXT, "
-    # Creating a new SQLite if it does not exist
 
-    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({fl})"\
-    .format(tn=table_name, fl=field_list[:-2]))
+        field_cleaned = re.sub(r'[^\w\s]', '', field)
+        field_cleaned = field_cleaned.replace(" ", "")
+        field_list += field_cleaned + " TEXT, "
+
+    c.execute("CREATE TABLE IF NOT EXISTS {tn} ({fl})"
+              .format(tn=table_name, fl=field_list[:-2]))
+
     inserts = ""
     for data in data:
+
         for level in data:
+
             insert_fields = "("
             for field, value in level.items():
-                insert_fields += "\"" +  value + "\", "
+                insert_fields += "\"" + value + "\", "
+
             inserts += insert_fields[:-2] + "), "
+
     insert_lines = inserts[:-2]
 
     try:
-        c.execute("INSERT INTO {tn} VALUES {iv}".\
-        format(tn=table_name, iv=insert_lines))
-    except Exception as e:
-        print("ERROR: Insert failed with" +  str(e))
+        c.execute("INSERT INTO {tn} VALUES {iv}".
+                  format(tn=table_name, iv=insert_lines))
+    except sqlite3.Error as error:
+        print("An error has occurred", error.args[0])
 
     conn.commit()
     conn.close()
 
+
 def select_from_db():
+
     conn = create_connection()
     c = conn.cursor()
     c.execute("SELECT * FROM screener_results")
+
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
 
     rows = c.fetchall()
 
