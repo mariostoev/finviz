a5783268efd4bbce64eea3c69cf254c594313ead
mariostoev <bg.mstoev@gmail.com>
2019-01-21 06:15:27 +0200

    Methods for individual stock symbols

--- finviz/__init__.py
+++ finviz/__init__.py
@@ -2,3 +2,4 @@ from finviz.screener import Screener
 from finviz.portfolio import Portfolio
 from finviz.main_func import get_stock
 from finviz.main_func import get_insider
+from finviz.main_func import get_news
--- finviz/main_func.py
+++ finviz/main_func.py
@@ -26,10 +26,10 @@ def get_stock(ticker):
 
 def get_insider(ticker):
     """
-    Returns a list of sets containing all recent insider transactions.
+    Returns a list of dictionaries containing all recent insider transactions.
 
     :param ticker: stock symbol
-    :return:
+    :return: list
     """
 
     page_parsed, _ = http_request_get(url=STOCK_URL, payload={'t': ticker}, parse=True)
@@ -38,3 +38,19 @@ def get_insider(ticker):
     data = [dict(zip(headers, row.xpath('td//text()'))) for row in table[1:]]
 
     return data
+
+
+def get_news(ticker):
+    """
+    Returns a list of sets containing news headlines and urls
+
+    :param ticker: stock symbol
+    :return: list
+    """
+
+    page_parsed, _ = http_request_get(url=STOCK_URL, payload={'t': ticker}, parse=True)
+    all_news = page_parsed.cssselect('a[class="tab-link-news"]')
+    headlines = [row.xpath('text()')[0] for row in all_news]
+    urls = [row.get('href') for row in all_news]
+
+    return list(zip(headlines, urls))
