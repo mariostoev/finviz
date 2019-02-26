965b23b42a480a57015c22c3d7e617947333a0c8
Minh Duc Le <ramboman777@gmail.com>
2019-02-26 15:21:20 -0500

    Fix setup.py: Bump version in download_url
    Bump version to 1.2.3 in download_url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
   author = 'Mario Stoev',
   author_email = 'bg.mstoev@gmail.com',
   url = 'https://github.com/mariostoev/finviz',
-  download_url = 'https://github.com/mariostoev/finviz/archive/v1.2.2.tar.gz',
+  download_url = 'https://github.com/mariostoev/finviz/archive/v1.2.3.tar.gz',
   keywords = ['finviz', 'api', 'screener', 'finviz api', 'charts', 'scraper'],
   install_requires=[
           'lxml',
