4b9aea1bc63224de1fa2c713bf6a3d7ca4e0215f
drbh <david.richard.holtz@gmail.com>
2021-12-12 10:22:45 -0500

    add env toggle for tqdm
    allow users to turn progress bar off via env var
    ```
    export DISABLE_TQDM=true
    ```

--- finviz/helper_functions/request_functions.py
+++ finviz/helper_functions/request_functions.py
@@ -1,6 +1,7 @@
 import asyncio
 from typing import Callable, Dict, List
 
+import os
 import aiohttp
 import requests
 import tenacity
@@ -62,7 +63,7 @@ def sequential_data_scrape(
 ) -> List[Dict]:
     data = []
 
-    for url in tqdm(urls):
+    for url in tqdm(urls, disable=os.environ.get("DISABLE_TQDM", False):
         try:
             response = finviz_request(url, user_agent)
             kwargs["URL"] = url
