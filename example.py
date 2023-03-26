202f98b9bc6b18f0e49ff5f0edbe70386c08132f
NAVNAV221 <navve2008@gmail.com>
2023-03-26 23:18:42 +0300

    Reload json filters from file

--- example.py
+++ example.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 
-from finviz.screener import Screener
+from API.finvizAPI.finviz.screener import Screener
 
 # Get dict of available filters
 # filters dict contains the corresponding filter tags
 filters = Screener.load_filter_dict()
 some_filters = [filters["PEG"]["Under 1"], filters["Exchange"]["AMEX"]]
+print(some_filters)
 stock_list = Screener(filters=some_filters, order="ticker")
-print(stock_list)
+print(stock_list.tickers())
 
 # Use raw filter tags in a list
 # filters = ['geo_usa']
-filters = ["idx_sp500"]  # Shows companies in the S&P500
-print("Screening stocks...")
-stock_list = Screener(filters=filters, order="ticker")
-print(stock_list)
+# filters = ["idx_sp500"]  # Shows companies in the S&P500
+# print("Screening stocks...")
+# stock_list = Screener(filters=filters, order="ticker")
+# print(stock_list)
 
-print("Retrieving stock data...")
-stock_data = stock_list.get_ticker_details()
-print(stock_data)
-
-# Export the screener results to CSV file
-stock_list.to_csv("sp500.csv")
-
-# Create a SQLite database
-# stock_list.to_sqlite("sp500.sqlite")
+# print("Retrieving stock data...")
+# stock_data = stock_list.get_ticker_details()
+# print(stock_data)
+#
+# # Export the screener results to CSV file
+# stock_list.to_csv("sp500.csv")
+#
+# # Create a SQLite database
+# # stock_list.to_sqlite("sp500.sqlite")
